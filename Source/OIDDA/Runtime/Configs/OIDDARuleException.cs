798ee0ad705242575ef458195837e11d38031b81
Phantom <salvini.michael01@gmail.com>
2025-12-30 13:13:18 +0100

    -u

---
+++ Source/OIDDA/Runtime/Configs/OIDDARuleException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace OIDDA;
+
+/// <summary>
+/// OIDDA Rule Exception
+/// </summary>
+public class OIDDARuleException : OIDDARule
+{
+
+}
