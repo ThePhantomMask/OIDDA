f73038711103652037e7004d9361f9064ffa383e
Phantom <salvini.michael01@gmail.com>
2026-01-11 03:36:57 +0100

    -u

--- Source/OIDDA/Runtime/OIDDASettings.cs
+++ Source/OIDDA/Runtime/OIDDASettings.cs
@@ -25,38 +25,6 @@ public class OIDDASettings : SettingsBase
     /// </summary>
     [EditorOrder(0), EditorDisplay("OIDDA Config"), Tooltip("Metrics update interval (seconds)")]
     public float UpdateInterval = 1.0f;
-
-    /// <summary>
-    /// The folder name that save the data file
-    /// </summary>
-    [EditorOrder(1), EditorDisplay("File Config")]
-    public string FolderName = "OIDDA";
-    /// <summary>
-    /// The file name to use.
-    /// </summary>
-    [EditorOrder(1), EditorDisplay("File Config")]
-    public string OIDDASaveName = "Data";
-    /// <summary>
-    /// Whether to encrypt the save data.
-    /// </summary>
-    [EditorOrder(1), EditorDisplay("File Config")]
-    public bool UseEncryption = false;
-    /// <summary>
-    /// The password used for encryption.
-    /// </summary>
-    [EditorOrder(1), EditorDisplay("File Config")]
-    public string Password = "password";
-    /// <summary>
-    /// Whether to add and use a hash to check for file changes.
-    /// </summary>
-    [EditorOrder(1), EditorDisplay("File Config")]
-    public bool UseHash = true;
-    /// <summary>
-    /// Whether to use verbose logging. Automatically disabled in release builds.
-    /// </summary>
-    [EditorOrder(1), EditorDisplay("File Config")]
-    public bool VerboseLogging = true;
-
     /// <summary>
     /// Collection of Static ORS (OIDDA Receiver Sender) agents for managing the OIDDA data.
     /// </summary>
