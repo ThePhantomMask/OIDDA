a08a94fae92cc72a1f0eaba312bfbd5a566732f8
Phantom <salvini.michael01@gmail.com>
2026-02-01 01:49:38 +0100

    Upd

---
+++ Source/OIDDA/Data/Director/Config/DirectorConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace OIDDA;
+
+/// <summary>
+/// DirectorConfig class.
+/// </summary>
+public class DirectorConfig
+{
+
+}
