39caa2f5a8bebec32de992f661fc2aba07c12308
Phantom <salvini.michael01@gmail.com>
2026-02-07 18:01:37 +0100

    -u Rule Exception

--- Source/OIDDA/Data/Configs/Config Components/OIDDARuleException.cs
+++ Source/OIDDA/Data/Configs/Config Components/OIDDARuleException.cs
@@ -13,8 +13,10 @@ public class OIDDARuleException : OIDDARule
     public string ExceptionName;
     public ExceptionType Type;
 
-    [NoSerialize] public new string RuleName;
+    [VisibleIf(nameof(_neverShow))] public new string RuleName;
     [NoSerialize] public new List<OIDDARuleException> Exceptions;
+
+    bool _neverShow = false;
 
     public override void Apply(Dictionary<string, object> metrics)
     {
