d408a8a323e6a3b49e38aba3156a0a069e3f4050
Phantom <salvini.michael01@gmail.com>
2026-03-01 11:38:01 +0100

    -u Director

--- Source/OIDDA/Runtime/Manager/Director Pacing/DirectorManager.cs
+++ Source/OIDDA/Runtime/Manager/Director Pacing/DirectorManager.cs
@@ -56,6 +56,7 @@ public class DirectorManager
         timeSinceLastPeak += deltaTime;
         StateTimer += deltaTime;
 
+        smoothingManager.SmoothUpdate(deltaTime);
         UpdatePsychologicalMetrics(deltaTime, GameplayValues);
         UpdatePacingState(StateTimer);
         ApplyIntensityDecay(deltaTime);
