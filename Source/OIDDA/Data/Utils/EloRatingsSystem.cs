488ec0db18c786d39cd51bc73ed304574e52f0be
Phantom <salvini.michael01@gmail.com>
2026-06-13 10:39:21 +0200

    Add Elo Ratings

---
+++ Source/OIDDA/Data/Utils/EloRatingsSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace OIDDA.Elo;
+
+/// <summary>
+/// Possible outcomes of a "match" from the player's perspective.
+/// </summary>
+public enum MatchResult
+{
+    Loss = 0,
+    Draw = 1,
+    Win = 2
+}
+
+/// <summary>
+/// EloRatingsSystem class.
+/// </summary>
+public class EloRatingsSystem
+{
+
+}
