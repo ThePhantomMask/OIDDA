5259bd4e393618a407850c540b39381ce31bd857
Phantom <salvini.michael01@gmail.com>
2026-06-20 14:05:43 +0200

    -u

--- Source/OIDDA/Runtime/OIDDASettings.cs
+++ Source/OIDDA/Runtime/OIDDASettings.cs
@@ -38,7 +38,7 @@ public class OIDDASettings : SettingsBase
     /// Collection of Static ORS (OIDDA Receiver Sender) agents for managing the OIDDA data.
     /// </summary>
     [EditorOrder(2), EditorDisplay("ORS Config")]
-    public List<StaticORSAgentEntry> StaticORSGroup;
+    public List<List<StaticORSAgentEntry>> StaticORSGroup;
     /// <summary>
     /// Delay for ORS Agents
     /// </summary>
